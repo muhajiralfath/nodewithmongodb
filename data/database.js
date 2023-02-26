ec1c76913e98df54ffe9f25030743149fc49e6a4
muhajiralfath <muhammadmuhajiralfath@gmail.com>
2023-02-26 15:05:42 +0700

    selesai fitur delete, edit dan try catch eror pada route

--- data/database.js
+++ data/database.js
@@ -5,13 +5,13 @@ const MongoClient = mongodb.MongoClient;
 let database;
 
 async function connect() {
-  const client = await MongoClient.connect("mongodb://localhost:27017");
+  const client = await MongoClient.connect("mongodb://127.0.0.1:27017");
   database = client.db("blog");
 }
 
 function getDb() {
   if (!database) {
-    throw { message: "Database Connection not Estabalished!!" };
+    throw { message: "Database connection not established!" };
   }
   return database;
 }
